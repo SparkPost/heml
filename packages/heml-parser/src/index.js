039de9715c6fcbcdf8e891ab6b2bbe4a2532cc6e
Avi Goldman <avrahamymgoldman@gmail.com>
2017-10-09 16:35:23 -0700

    built out the parser

---
+++ packages/heml-parser/src/index.js
@@ -0,0 +1,60 @@
+import { load } from 'cheerio'
+import { difference } from 'lodash'
+import htmlTags from 'html-tags'
+import selfClosingHtmlTags from 'html-tags/void'
+
+const wrappingHtmlTags = difference(htmlTags, selfClosingHtmlTags)
+
+function parser (contents, options = {}) {
+  const {
+    ignoreAttr = 'heml-ignore',
+    elements = [],
+    cheerio: cheerioOptions
+  } = options
+
+  const $ = load(contents, {
+    xmlMode: true,
+    lowerCaseTags: true,
+    decodeEntities: false,
+    ...cheerioOptions
+  })
+
+  $.findNodes = function (q) {
+    return $(Array.isArray(q) ? q.join(',') : q)
+            .not(`[${ignoreAttr}]`)
+            .toNodes()
+  }
+
+  $.prototype.toNodes = function () {
+    return this
+            .toArray()
+            .map((node) => $(node))
+  }
+
+  const selfClosingTags = [
+    ...selfClosingHtmlTags,
+    ...elements.filter((element) => element.children === false) ]
+  const wrappingTags = [
+    ...wrappingHtmlTags,
+    ...elements.filter((element) => element.children !== false) ]
+
+  const selfClosingNodes = $.findNodes(selfClosingTags).reverse()
+  const wrappingNodes = $.findNodes(wrappingTags).reverse()
+
+  /** Move contents from self wrapping tags outside of itself */
+  selfClosingNodes.forEach(($node) => {
+    $node.after($node.html())
+    $node.html('')
+  })
+
+  /** ensure that all wrapping tags have at least a single space */
+  wrappingNodes.forEach(($node) => {
+    if ($node.html().length === 0) {
+      $node.text(' ')
+    }
+  })
+
+  return $
+}
+
+export default parser
