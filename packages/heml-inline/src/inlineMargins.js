80ce20b87ce27eb2286adc86ea5c16d8a2bf9ff3
Avi Goldman <avrahamymgoldman@gmail.com>
2017-10-09 21:28:55 -0700

    inline margins to align prop

---
+++ packages/heml-inline/src/inlineMargins.js
@@ -0,0 +1,74 @@
+import { compact, first, last, nth } from 'lodash'
+
+/**
+ * finds all the tables that are centered with margins
+ * and centers them with the align attribute
+ * @param  {Cheerio} $
+ */
+function inlineMargins ($) {
+  $('table[style*=margin]').toNodes().forEach(($el) => {
+    const { left, right } = getSideMargins($el.attr('style'))
+
+    if (left === 'auto' && right === 'auto') {
+      $el.attr('align', 'center')
+    } else if (left === 'auto' && right !== 'auto') {
+      $el.attr('align', 'right')
+    } else if (left !== 'auto') {
+      $el.attr('align', 'left')
+    }
+  })
+}
+
+/**
+ * pulls the left and right margins from the given inline styles
+ * @param  {String} style the inline styles
+ * @return {Object}       object with left and right margins
+ */
+function getSideMargins (style) {
+  const margins = compact(style.split(';')).map((decl) => {
+    const split = decl.split(':')
+
+    return {
+      prop: first(split).trim().toLowerCase(),
+      value: last(split).trim().toLowerCase()
+    }
+  }).filter({ prop, value }) => prop.indexOf('margin') === 0)
+
+
+  let left = 0, right = 0
+  margins.forEach(({ prop, value }) => {
+    if (prop === 'margin-left') { return left = value }
+
+    if (prop === 'margin-right') { return right = value }
+
+    if (prop === 'margin') {
+      const values = _.map(value.split(' '), _.trim)
+
+      switch (values.length) {
+        case 1:
+          right = first(values)
+          left = first(values)
+          break;
+
+        case 2:
+          right = last(values)
+          left = last(values)
+          break;
+
+        case 3:
+          right = nth(values, 1)
+          left = nth(values, 1)
+          break;
+
+        default:
+          right = nth(values, 1)
+          left = nth(values, 3)
+          break;
+      }
+    }
+  })
+
+  return { left, right }
+}
+
+export default inlineMargins
