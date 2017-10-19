577e6bdfeae5b920ebe457b78c67910d56acbe99
Avi Goldman <avrahamymgoldman@gmail.com>
2017-10-18 23:18:39 -0700

    export the ol' fashion way

--- packages/heml-utils/src/index.js
+++ packages/heml-utils/src/index.js
@@ -3,4 +3,4 @@ import createElement from './createElement'
 import HEMLError from './HEMLError'
 import utils from './utils'
 
-export { createElement, renderElement, HEMLError, utils }
+module.exports = { createElement, renderElement, HEMLError, utils }
