61828f333d4c034b951d51abe3f5a06b0b3f223d
Avi Goldman <avrahamymgoldman@gmail.com>
2017-10-18 19:02:35 -0700

    added HEML Error

---
+++ packages/heml/src/Error.js
@@ -0,0 +1,73 @@
+import { min, max } from 'lodash'
+
+export default class HEMLError extends Error {
+  constructor (message, $node) {
+    super(message)
+    this.name = 'HEMLError'
+
+    if ($node) {
+      this.$node = $node
+    }
+
+    Error.captureStackTrace(this, HEMLError)
+  }
+
+  get selector () {
+    if (!this.$node) { return }
+
+    const nodeSelector = buildSelector(this.$node[0])
+    const strSelector = this.$node.parents()
+      .map((index, node) => buildSelector(node))
+      .toArray()
+      .reverse()
+      .concat([nodeSelector])
+      .join(' > ')
+
+    const chopAfter = min(max(0, strSelector.lastIndexOf('#')),
+      max(0, strSelector.lastIndexOf('html')),
+      max(0, strSelector.lastIndexOf('heml')))
+
+    return strSelector.substr(chopAfter)
+  }
+}
+
+function buildSelector (node) {
+  if (node.id) {
+    return `#${node.id}`
+  }
+
+  const tag = node.tagName.toLowerCase()
+  const siblingsBefore = findSiblingsBefore(node)
+  const siblingsAfter = findSiblingsAfter(node)
+  const siblings = siblingsBefore.concat(siblingsAfter)
+
+  const sameTag = siblings.filter((s) => s.tagName.toLowerCase() === tag)
+
+  if (siblings.length === 0 || sameTag.length === 0) {
+    return tag
+  }
+
+  const sameTagAndClass = siblings.filter((s) => s.className === node.className && s.tagName.toLowerCase() === tag)
+
+  if (node.className && sameTagAndClass.length === 0) {
+    return `${tag}.${node.className.split(' ').join('.')}`
+  }
+
+  return `${tag}:nth-child(${siblingsBefore.length + 1})`
+}
+
+function findSiblingsBefore (node, siblings = []) {
+  if (!node.previousSibling) { return siblings }
+
+  if (node.previousSibling.tagName) { siblings = siblings.concat([node.previousSibling]) }
+
+  return findSiblingsBefore(node.previousSibling, siblings)
+}
+
+function findSiblingsAfter (node, siblings = []) {
+  if (!node.nextSibling) { return siblings }
+
+  if (node.nextSibling.tagName) { siblings = siblings.concat([node.nextSibling]) }
+
+  return findSiblingsAfter(node.nextSibling, siblings)
+}
