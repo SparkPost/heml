2745a3236a3b60bafa8a61251c67bf47687d022e
Avi Goldman <avrahamymgoldman@gmail.com>
2017-10-22 15:15:09 -0700

    added a small validation around providing a name for a custom element

--- packages/heml-utils/src/createElement.js
+++ packages/heml-utils/src/createElement.js
@@ -1,12 +1,16 @@
 import { defaults, isFunction } from 'lodash'
 
 export default function (name, element) {
+  if (!name || name.trim().length === 0) {
+    throw new Error(`When creating an element, you must set the name. ${name.trim().length === 0 ? 'An empty string' : `"${name}"`} was given.`)
+  }
+
   if (isFunction(element)) {
     element = { render: element }
   }
 
-  element = defaults({}, element, {
-    tagName: name,
+  element = defaults({}, element || {}, {
+    tagName: name.trim().toLowerCase(),
     attrs: [],
     children: true,
     defaultAttrs: {},
