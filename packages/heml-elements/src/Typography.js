8995008be08a9e3f43b660f5eb6d44f577fa54d0
Avi Goldman <avrahamymgoldman@gmail.com>
2017-10-25 09:29:33 -0700

    added all typography elemenets - see for containsText in createElement for class reasons

---
+++ packages/heml-elements/src/Typography.js
@@ -0,0 +1,60 @@
+import HEML, { createElement, utils } from '@heml/utils' // eslint-disable-line no-unused-vars
+import { merge } from 'lodash'
+
+const {
+  margin, background, border, borderRadius, text, font
+} = utils.cssGroups
+
+/**
+ * create mergable text element
+ * @param  {String} name
+ * @param  {Object} element
+ * @return {Object}
+ */
+function createTextElement (name, element = {}) {
+  return createElement(name, merge({
+    attrs: true,
+    rules: {
+      [`${name}`]: [ { '@pseudo': 'root' }, '@default', { display: utils.trueHide() }, margin, background, border, borderRadius, text, font ]
+    },
+    render (attrs, contents) {
+      const Tag = name
+
+      if (/^h\d$/i.test(name)) {
+        attrs.class += ' header'
+      } else {
+        attrs.class += ' text'
+      }
+
+      return <Tag {...attrs}>{contents}</Tag>
+    }
+  }, element))
+}
+
+const H1 = createTextElement('h1')
+const H2 = createTextElement('h2')
+const H3 = createTextElement('h3')
+const H4 = createTextElement('h4')
+const H5 = createTextElement('h5')
+const H6 = createTextElement('h6')
+const P = createTextElement('p')
+const Ol = createTextElement('ol')
+const Ul = createTextElement('ul')
+const Li = createTextElement('li')
+
+const A = createElement('a', {
+  attrs: true,
+  defaultAttrs: { href: '#' },
+
+  rules: {
+    '.a': [ { '@pseudo': 'root' }, { '@default': true }, { display: utils.trueHide('inline') }, 'color', 'text-decoration' ],
+    '.a__text': [ { '@pseudo': 'text' }, 'color', 'text-decoration' ]
+  },
+
+  render (attrs, contents) {
+    attrs.class += ' a'
+    return <a {...attrs}><span class='a__text'>{contents}</span></a>
+  }
+})
+
+export { H1, H2, H3, H4, H5, H6, P, Ol, Ul, Li, A }
