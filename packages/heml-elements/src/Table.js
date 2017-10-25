d16e3cd4e32d4fd2b8535570f1f6671fc93098ea
Avi Goldman <avrahamymgoldman@gmail.com>
2017-10-25 09:18:54 -0700

    added table element so styles don't collide

---
+++ packages/heml-elements/src/Table.js
@@ -0,0 +1,42 @@
+import HEML, { createElement, utils } from '@heml/utils' // eslint-disable-line no-unused-vars
+
+const Table = createElement('table', {
+  attrs: true,
+  containsText: true,
+  rules: {
+    '.table': [ { '@pseudo': 'root' }, '@default', { display: utils.trueHide('table') } ]
+  },
+
+  render (attrs, contents) {
+    attrs.class += ' table'
+    return <table {...attrs}>{contents}</table>
+  }
+})
+
+const Tr = createElement('tr', {
+  attrs: true,
+  containsText: true,
+  rules: {
+    '.tr': [ { '@pseudo': 'root' }, '@default' ]
+  },
+
+  render (attrs, contents) {
+    attrs.class += ' tr'
+    return <tr {...attrs}>{contents}</tr>
+  }
+})
+
+const Td = createElement('td', {
+  attrs: true,
+  containsText: true,
+  rules: {
+    '.td': [ { '@pseudo': 'root' }, '@default' ]
+  },
+
+  render (attrs, contents) {
+    attrs.class += ' td'
+    return <td {...attrs}>{contents}</td>
+  }
+})
+
+export { Table, Tr, Td }
