edf59af8d01c2adbd9e507a7ff1481719489eda7
Avi Goldman <avrahamymgoldman@gmail.com>
2017-10-25 09:29:49 -0700

    export all the elements

---
+++ packages/heml-elements/src/index.js
@@ -0,0 +1,69 @@
+/* Document */
+import Heml from './Heml'
+import Head from './Head'
+import Body from './Body'
+
+/* Meta */
+import Meta from './Meta'
+import Subject from './Subject'
+import Preview from './Preview'
+import Base from './Base'
+import Font from './Font'
+import Style from './Style'
+
+/* Table */
+import { Table, Tr, Td } from './Table'
+
+/* Grid */
+import Block from './Block'
+import Container from './Container'
+import Row from './Row'
+import Column from './Column'
+
+/* Content */
+import { H1, H2, H3, H4, H5, H6, P, Ol, Ul, Li, A } from './Typography'
+import Hr from './Hr'
+import Button from './Button'
+import Img from './Img'
+
+export {
+  /* Document */
+  Heml,
+  Head,
+  Body,
+
+  /* Meta */
+  Meta,
+  Subject,
+  Preview,
+  Base,
+  Font,
+  Style,
+
+  /* Table */
+  Table,
+  Tr,
+  Td,
+
+  /* Grid */
+  Block,
+  Container,
+  Row,
+  Column,
+
+  /* Content */
+  H1,
+  H2,
+  H3,
+  H4,
+  H5,
+  H6,
+  P,
+  Ol,
+  Ul,
+  Li,
+  A,
+  Hr,
+  Button,
+  Img
+}
