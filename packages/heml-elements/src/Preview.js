b3558aaaac4e0cb47bdc32e68e79d6bd0b2ce4bf
Avi Goldman <avrahamymgoldman@gmail.com>
2017-10-25 09:21:40 -0700

    added preview

---
+++ packages/heml-elements/src/Preview.js
@@ -0,0 +1,19 @@
+import HEML, { createElement } from '@heml/utils' // eslint-disable-line no-unused-vars
+import Meta from './Meta'
+
+export default createElement('preview', {
+  parent: [ 'head' ],
+  unique: true,
+
+  render (attrs, contents) {
+    Meta.set('preview', contents)
+
+    return false
+  },
+
+  flush () {
+    const preview = Meta.get('preview')
+
+    return preview ? <div class='preview' style='display: none; font-size: 1px; line-height: 1px; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden; mso-hide: all;'>{preview}{'&nbsp; '.repeat(100 - preview.length)}</div> : ''
+  }
+})
