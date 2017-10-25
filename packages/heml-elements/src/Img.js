b3641a68404129b55f49b92adec6f63c07c05864
Avi Goldman <avrahamymgoldman@gmail.com>
2017-10-25 09:13:49 -0700

    added working image

---
+++ packages/heml-elements/src/Img.js
@@ -0,0 +1,64 @@
+import HEML, { createElement, utils } from '@heml/utils' // eslint-disable-line no-unused-vars
+import Style from './Style'
+import { omit, has } from 'lodash'
+import fs from 'fs-extra'
+import isAbsoluteUrl from 'is-absolute-url'
+import axios from 'axios'
+import sizeOf from 'image-size'
+
+export default createElement('img', {
+  attrs: [ 'src', 'width', 'height', 'alt', 'infer', 'inline', 'style' ],
+  children: false,
+  defaultAttrs: {
+    border: '0',
+    alt: ''
+  },
+
+  rules: {
+    'img': [ { '@pseudo': 'root' }, { display: utils.trueHide() }, '@default' ]
+  },
+
+  async render (attrs, contents) {
+    const isBlock = !attrs.inline
+
+    if (!!attrs.infer && has(attrs, 'src') && !attrs.width) {
+      attrs.width = await getWidth(attrs.src, attrs.infer === 'retina')
+    }
+
+    attrs.class += ` ${isBlock ? 'img__block' : 'img__inline'}`
+    attrs.style = isBlock ? '' : 'display: inline-block;'
+
+    return ([
+      <img {...omit(attrs, 'inline', 'infer')} />,
+      <Style for='img'>{`
+        .img__block {
+          display: block;
+          max-width: 100%;
+        }
+      `}</Style>])
+  }
+})
+
+async function getWidth (path, isRetina) {
+  try {
+    const image = await (isAbsoluteUrl(path) ? getRemoteBuffer(path) : fs.readFile(path))
+
+    const { width } = sizeOf(image)
+    if (!width) { return 'auto' }
+
+    return isRetina ? Math.round(width / 2) : width
+  } catch (e) {
+    return 'auto' // if we fail fall back to auto
+  }
+}
+
+function getRemoteBuffer (path) {
+  return axios({
+    method: 'get',
+    url: path,
+    responseType: 'arraybuffer'
+  })
+  .then(({ data }) => {
+    return Buffer.from(data, 'binary')
+  })
+}
