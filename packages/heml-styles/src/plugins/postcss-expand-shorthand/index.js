adc66c38f85aac074a42552a1f9baa04560c1fc5
Avi Goldman <avrahamymgoldman@gmail.com>
2017-10-25 20:59:54 -0700

    fixed shorthand expand bug

--- packages/heml-styles/src/plugins/postcss-expand-shorthand/index.js
+++ packages/heml-styles/src/plugins/postcss-expand-shorthand/index.js
@@ -3,7 +3,7 @@ import shorthandExpand from 'css-shorthand-expand'
 
 export default postcss.plugin('postcss-expand-shorthand', () => (root) => {
   root.walkDecls((decl) => {
-    if (shouldExpand(decl.prop)) {
+    if (shouldExpand(decl.prop) && !!decl.value) {
       const expandedDecls = shorthandExpand(decl.prop, decl.value)
 
       for (const [ prop, value ] of Object.entries(expandedDecls)) {
