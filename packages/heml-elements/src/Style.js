037e261849213751730fb56499d4462a2c233445
Avi Goldman <avrahamymgoldman@gmail.com>
2017-12-01 18:28:22 -0800

    refactored Style element to process the css before rendering

--- packages/heml-elements/src/Style.js
+++ packages/heml-elements/src/Style.js
@@ -1,124 +1,116 @@
 import HEML, { createMetaElement } from '@heml/utils' // eslint-disable-line no-unused-vars
 import hemlstyles from '@heml/styles'
-import { castArray, isEqual, uniqWith, sortBy } from 'lodash'
+import escape from 'escape-string-regexp'
+import { castArray, isEqual, uniqWith, compact } from 'lodash'
 
-const START_EMBED_CSS = `/*!***START:EMBED_CSS*****/`
-const START_INLINE_CSS = `/*!***START:INLINE_CSS*****/`
-
-let styleMap
-let options
+const COMMENT_EMBED_CSS = `/*!***EMBED_CSS*****/`
+const COMMENT_INLINE_CSS = `/*!***INLINE_CSS*****/`
+const COMMENT_REGEX = new RegExp(`(${escape(COMMENT_EMBED_CSS)}|${escape(COMMENT_INLINE_CSS)})`)
 
 export default createMetaElement('style', {
   parent: [ 'head' ],
-  attrs: [ 'for', 'heml-embed' ],
-  defaultAttrs: {
-    'heml-embed': false,
-    'for': 'global'
-  },
+  attrs: [ 'heml-embed' ],
+  defaultAttrs: { 'heml-embed': false },
 
-  preRender ({ $, elements }) {
-    styleMap = new Map([ [ 'global', [] ] ])
-    options = {
-      plugins: [],
-      elements: {},
-      aliases: {}
-    }
+  async preRender (globals) {
+    const { $ } = globals
+    const options = buildOptions(globals)
+    const $nodes = $.findNodes('style')
+    const styles = nodesToStyles($nodes)
 
-    for (let element of elements) {
-      if (element.postcss) {
-        options.plugins = options.plugins.concat(castArray(element.postcss))
-      }
+    const { css: processedCss } = await hemlstyles(stylesToCss(styles), options)
+    const processedStyles = cssToStyles(processedCss)
 
-      if (element.rules) {
-        options.elements[element.tagName] = element.rules
-      }
-
-      options.aliases[element.tagName] = $.findNodes(element.tagName)
-    }
+    $nodes.forEach(($node) => $node.remove())
+    $('head').append(stylesToTags(processedStyles))
   },
 
   render (attrs, contents) {
-    if (!styleMap.get(attrs.for)) {
-      styleMap.set(attrs.for, [])
-    }
-
-    styleMap.get(attrs.for).push({
-      embed: !!attrs['heml-embed'],
-      ignore: !!attrs['heml-ignore'],
-      css: contents
-    })
-
-    return false
-  },
-
-  async flush () {
-    /**
-     * reverse the styles so they fall in an order that mirrors their position
-     * - they get rendered bottom to top - should be styled top to bottom
-     *
-     * the global styles should always be rendered last
-     */
-    const globalStyles = styleMap.get('global')
-    styleMap.delete('global')
-    styleMap = new Map([...styleMap].reverse())
-    styleMap.set('global', globalStyles)
-
-    let ignoredCSS = []
-    let fullCSS = ''
-
-    /** combine the non-ignored css to be combined */
-    for (let [ element, styles ] of styleMap) {
-      styles = uniqWith(styles, isEqual)
-      styles = element === 'global' ? styles : sortBy(styles, ['embed', 'css'])
-
-      styles.forEach(({ ignore, embed, css }) => {
-        /** replace the ignored css with placeholders that will be swapped later */
-        if (ignore) {
-          ignoredCSS.push({ embed, css })
-          fullCSS += ignoreComment(ignoredCSS.length - 1)
-        } else if (embed) {
-          fullCSS += `${START_EMBED_CSS}${css}`
-        } else {
-          fullCSS += `${START_INLINE_CSS}${css}`
-        }
-      })
-    }
-
-    let { css: processedCss } = await hemlstyles(fullCSS, options)
-
-    /** put the ignored css back in */
-    ignoredCSS.forEach(({ embed, css }, index) => {
-      processedCss = processedCss.replace(ignoreComment(index), embed ? `${START_EMBED_CSS}${css}` : `${START_INLINE_CSS}${css}`)
-    })
-
-    /** split on the dividers and map it so each part starts with INLINE or EMBED */
-    let processedCssParts = processedCss.split(/\/\*!\*\*\*START:/g).splice(1).map((css) => css.replace(/_CSS\*\*\*\*\*\//, ''))
-
-    /** build the html */
-    let html = ''
-    let lastType = null
-
-    for (let cssPart of processedCssParts) {
-      const css = cssPart.replace(/^(EMBED|INLINE)/, '')
-      const type = cssPart.startsWith('EMBED') ? 'EMBED' : 'INLINE'
-
-      if (type === lastType) {
-        html += css
-      } else {
-        lastType = type
-        html += `${html === '' ? '' : '</style>'}\n<style${type === 'EMBED' ? ' data-embed' : ''}>${css}\n`
-      }
-    }
-
-    html += '</style>'
-
-    /** reset the styles and options */
-    styleMap = options = null
-
-    return html
+    return true
   }
 })
 
-function ignoreComment (index) {
-  return `/*!***IGNORE_${index}*****/`
+
+/**
+ * Generates the options object to be passed to hemlstyles
+ * @param  {Cheerio} $        A cheerio instance
+ * @param  {Array}   elements an array of HEML elements
+ * @return {Object}           options for hemlstyles
+ */
+function buildOptions ({ $, elements }) {
+  const options = {
+    plugins: [],
+    elements: {},
+    aliases: {}
+  }
+
+  for (let element of elements) {
+    if (element.postcss) {
+      options.plugins = [ options.plugins, ...castArray(element.postcss) ]
+    }
+
+    if (element.rules) {
+      options.elements[element.tagName] = element.rules
+    }
+
+    options.aliases[element.tagName] = $.findNodes(element.tagName)
+  }
+
+  return options
+}
+
+/**
+ * Converts an array of $nodes into style objects
+ * scope = { embed: Boolean, css: String }
+ * @param  {Array[Cheerio]} $nodes
+ * @return {Array[Object]}
+ */
+function nodesToStyles($nodes) {
+  return $nodes.map(($node) => {
+    return { css: $node.html(), embed: $node.is('[heml-embed]') }
+  })
+}
+
+/**
+ * Converts an array of style objects into a single string to be processed by hemlstyles
+ * @param  {Array[Object]} styles
+ * @return {String}
+ */
+function stylesToCss(styles) {
+  styles = uniqWith(styles, isEqual)
+
+  return styles.reduce((fullCss, { embed, css }) => fullCss += embed ?
+    `${COMMENT_EMBED_CSS}${css}` : `${COMMENT_INLINE_CSS}${css}`, '')
+}
+
+/**
+ * Converts a css string into  array of style objects
+ * @param  {String}          css
+ * @return {Array[Object]}   styles
+ */
+function cssToStyles(css) {
+  const parts = compact(css.split(COMMENT_REGEX))
+
+  return parts.reduce((styles, value) => {
+    if (value === COMMENT_EMBED_CSS) {
+      return [ ...styles, { embed: true, css: '' } ]
+    } else if (value === COMMENT_INLINE_CSS) {
+      return [ ...styles, { embed: false, css: '' } ]
+    } else {
+      styles[styles.length - 1].css += value
+
+      return styles
+    }
+  }, []).filter((style) => style.css.length > 0)
+}
+
+/**
+ * Convert style objects into html tags
+ * @param  {Array[Object]} styles
+ * @return {Array[String]}
+ */
+function stylesToTags(styles) {
+  return styles.map(({ embed, css }) => {
+    return `<style ${embed ? 'data-embed' : ''}>${css}</style>\n`
+  })
 }
