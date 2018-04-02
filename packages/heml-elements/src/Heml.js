5845ee4b0c299e274d860b43722c9225de093565
M.K. Safi <msafi@msafi.com>
2018-04-02 16:20:23 -0700

    Update Heml.js

--- packages/heml-elements/src/Heml.js
+++ packages/heml-elements/src/Heml.js
@@ -13,7 +13,7 @@ export default createElement('heml', {
 
   render (attrs, contents) {
     return ([
-      `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd" />`,
+      `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">`,
       <html {...attrs}>
         {contents}
       </html>])
